7fd3e261545cb62428caf728423153abbcf95170
Saikrishna <syadav7@uic.edu>
2020-11-25 20:14:25 -0600

    Completion of Day 1

---
+++ Projects/Day_1/main.py
@@ -0,0 +1,7 @@
+def bandNameGenerator():
+    print("Hello, I hope you are doing well!")
+    city = input("What city did you grow up in? ")
+    pet = input("What is the name of your pet? ")
+    print('Awesome, your band name is ' + str(city) + " " + str(pet))
+
+bandNameGenerator()
